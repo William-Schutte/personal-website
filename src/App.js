c127c85933400b0d01e1ff8db5cf7e75f73a46c8
William Schutte <williamdschutte@gmail.com>
2020-11-10 17:03:41 -0500

    Complete restart on new portfolio, init react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="header__bg-gradient">
+          <div className="header__row">
+            <div className="header__text">
+              <h1 className="header__name">William Schutte</h1>
+              <h2 className="header__sub">Full Stack Web Developer</h2>
+            </div>
+            <button className="header__btn" />
+          </div>
+        </div>
+        
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
