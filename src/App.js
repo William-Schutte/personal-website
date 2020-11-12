984238df620bfa08c32b1365621f9bdcd3a9a762
William Schutte <williamdschutte@gmail.com>
2020-11-11 19:24:49 -0500

    Projects sections and contact section outlined

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import triangles from './images/bg-triangles.png';
 import iconCss from './images/i-css.svg';
 import iconExpress from './images/i-express.svg';
 import iconGithub from './images/i-github.svg';
@@ -8,6 +7,8 @@ import iconMongo from './images/i-mongo.svg';
 import iconNode from './images/i-node.svg';
 import iconNpm from './images/i-npm.svg';
 import iconReact from './images/i-react.svg';
+import iconMail from './images/i-mail.png'
+import iconMobile from './images/i-mobile.png'
 
 
 
@@ -30,55 +31,102 @@ function App() {
         <div className="about__me">
           <h3 className="about__title">Who am I?</h3>
           <p className="about__me-text">I'm an adaptable developer and engineer with a passion for mathematics and science.</p>
-          <p className="about__me-text">I have a full-stack web-dev training program <br></br>under my belt, where I became proficient 
+          <p className="about__me-text">I have a full-stack web-dev training program <br></br>under my belt, where I became proficient
             <br></br>with MERN stack tech including: <span className="bold">React, <br></br>Node, Express, MongoDB, Microsoft <br></br> Azure, </span>
               and <span className="bold">JS/HTML/CSS</span>.</p>
         </div>
-
         <div className="about__tech">
           <h3 className="about__title">Technologies</h3>
           <ul className="about__tech-icons-block">
             <li className="about__tech-icon">
-              <img className="about__tech-icon-img" src={iconReact}/>
+              <img className="about__tech-icon-img" src={iconReact} />
               <p className="about__tech-icon-name">React</p>
             </li>
             <li className="about__tech-icon">
-              <img className="about__tech-icon-img" src={iconExpress}/>
+              <img className="about__tech-icon-img" src={iconExpress} />
               <p className="about__tech-icon-name">express.js</p>
             </li>
             <li className="about__tech-icon">
-              <img className="about__tech-icon-img" src={iconMongo}/>
+              <img className="about__tech-icon-img" src={iconMongo} />
               <p className="about__tech-icon-name">MongoDB</p>
             </li>
             <li className="about__tech-icon">
-              <img className="about__tech-icon-img" src={iconNode}/>
+              <img className="about__tech-icon-img" src={iconNode} />
               <p className="about__tech-icon-name">Node.js</p>
             </li>
-            <li className="about__tech-icon">
+            {/* <li className="about__tech-icon">
               <img className="about__tech-icon-img" src={iconNpm}/>
               <p className="about__tech-icon-name">npm</p>
-            </li>
+            </li> */}
             <li className="about__tech-icon">
-              <img className="about__tech-icon-img" src={iconGithub}/>
+              <img className="about__tech-icon-img" src={iconGithub} />
               <p className="about__tech-icon-name">GitHub</p>
             </li>
             <li className="about__tech-icon">
-              <img className="about__tech-icon-img-alt" src={iconJs}/>
+              <img className="about__tech-icon-img-alt" src={iconJs} />
               <p className="about__tech-icon-name">JavaScript</p>
             </li>
             <li className="about__tech-icon">
-              <img className="about__tech-icon-img" src={iconCss}/>
+              <img className="about__tech-icon-img" src={iconCss} />
               <p className="about__tech-icon-name">CSS</p>
             </li>
             <li className="about__tech-icon">
-              <img className="about__tech-icon-img" src={iconHtml}/>
+              <img className="about__tech-icon-img" src={iconHtml} />
               <p className="about__tech-icon-name">HTML</p>
             </li>
           </ul>
         </div>
-
       </section>
 
+      <section className="content">
+
+        <div className="projects">
+          <h2 className="projects__title">Projects</h2>
+          <div className="projects__container">
+            <ul className="projects__block">
+              <li className="projects__item">
+
+              </li>
+              <li className="projects__item">
+
+              </li>
+              <li className="projects__item">
+
+              </li>
+              <li className="projects__item">
+
+              </li>
+              <li className="projects__item">
+
+              </li>
+              <li className="projects__item">
+
+              </li>
+            </ul>
+          </div>
+
+        </div>
+        <div className="contact">
+          <h2 className="contact__title">Contact Me</h2>
+          <ul className="contact__container">
+            <li className="contact__item">
+              <img className="contact__icon" src={iconMail} />
+              <p className="contact__text">WilliamDSchutte<br></br>@gmail.com</p>
+            </li>
+            <li className="contact__item">
+              <img className="contact__icon" src={iconMobile} />
+              <p className="contact__text">+1 (305) 877 - 8459</p>
+            </li>
+            <li className="contact__item">
+              <img className="contact__icon" src={iconGithub} />
+              <p className="contact__text">/William-Schutte</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <footer className="footer">
+
+      </footer>
     </div>
   );
 }
