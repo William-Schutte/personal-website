602e44a402261171604f5847474b5799111249f7
William Schutte <williamdschutte@gmail.com>
2020-12-01 21:08:54 -0500

    Revise bio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,9 +3,10 @@ function AboutMe() {
         <div className="about__me">
           <h3 className="about__title">Who am I?</h3>
           <p className="about__me-text">I'm an adaptable developer and engineer with a passion for mathematics and science.</p>
-          <p className="about__me-text">I have a full-stack web-dev training program <br></br>under my belt, where I became proficient
-            <br></br>with MERN stack tech including: <span className="bold">React, <br></br>Node, Express, MongoDB, Microsoft <br></br> Azure, </span>
-              and <span className="bold">JS/HTML/CSS</span>.</p>
+          <p className="about__me-text">I have expertise with both frontend and backend<br></br> web dev frameworks. I love working
+            with MERN stack <br></br>tech including: <span className="bold">React, Node, Express, MongoDB, </span>
+            <br></br>and <span className="bold">Javascript</span>.</p>
+          <p className="about__me-text">I have projects deployed across multiple <br></br>cloud service providers like <span className="bold">Microsoft <br></br>Azure</span> and <span className="bold">AWS</span>.</p>
         </div>
     );
 }
