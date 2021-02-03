10550f5620e6fb5fe2eba57e9941e36e8a7767c9
William Schutte <williamdschutte@gmail.com>
2021-02-02 20:07:58 -0500

    Add Iterative.ai project

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -18,6 +18,10 @@ import portland2 from '../images/portfolio/portland2.png';
 import adventur1 from '../images/portfolio/adventur1.png';
 import adventur2 from '../images/portfolio/adventur2.jpg';
 import adventur3 from '../images/portfolio/adventur3.jpg';
+import iterative1 from '../images/portfolio/iterative1.png';
+import iterative2 from '../images/portfolio/iterative2.png';
+import iterative3 from '../images/portfolio/iterative3.png';
+import iterative4 from '../images/portfolio/iterative4.png';
 
 import css from '../images/icons/i-css.svg';
 import express from '../images/icons/i-express.svg';
@@ -46,18 +50,30 @@ class Project {
 }
 
 const p0 = new Project({ 
-    name: "News Explorer Project", 
+    name: "Iterative.ai Landing Page", 
+    ghLink: "https://github.com/William-Schutte/iterative-landing",
+    demoLink: "https://iterative.ai/",
+    description: [
+        "Iterative.ai, a data science and machine learning version control platform, came to us wanting a new static landing page built with Gatsby.", 
+        "I spearheaded a team of bootcamp graduates to develop and ship this site in about 3 weeks. We organized tasks on Trello and collaborated via Github to pull all the components together.",
+        "We decided to use Styled Components as this design framework pairs nicely with Gatsby."],
+    tech: [gatsby, react, js, github],
+    imgs: [iterative1, iterative2, iterative3, iterative4],
+});
+
+const p1 = new Project({ 
+    name: "News Explorer", 
     ghLink: "https://github.com/William-Schutte/news-explorer-frontend",
     demoLink: "https://ws.news.students.nomoreparties.site/",
     description: [
-        "News Explorer is a React application that fetches articles from a powerful NewsAPI and displays articles for the user. Once logged in, users can save articles to their profile.", 
-        "This fully responsive SPA was built with Create-React-App, served via an EC2 machine on AWS. The backend is also on the same server at the API subdomain, running Node.js/Express.",
+        "News Explorer is a React application that fetches articles from the powerful NewsAPI and displays articles for the user. Once logged in, users can save articles to their profile.", 
+        "This fully responsive SPA was built with Create-React-App in Agile-like 2 week sprints with tasks self-assigned on a Jira Kanban board, served via an EC2 machine on AWS. The backend is also on the same server at the API subdomain, running Node.js/Express.",
         "MongoDB stores user and article data, while JWTs are used to keep users logged in and to remember searched articles when returning to the page."],
     tech: [react, express, mongo, js, node, npm],
     imgs: [news1, news2, news3, news4],
 });
 
-const p1 = new Project({ 
+const p2 = new Project({ 
     name: "Indubitably", 
     ghLink: "https://github.com/William-Schutte/indubitably",
     demoLink: "http://ws-indubitably.com/",
@@ -69,8 +85,8 @@ const p1 = new Project({
     imgs: [indubitably1, indubitably2, indubitably3],
 });
 
-const p2 = new Project({ 
-    name: "React Image-Share Project", 
+const p3 = new Project({ 
+    name: "Around the U.S. Image-Share Project", 
     ghLink: "https://github.com/William-Schutte/react-around-api-full",
     demoLink: "https://ws.p15.students.nomoreparties.site/",
     description: [
@@ -81,7 +97,7 @@ const p2 = new Project({
     imgs: [around1, around2, around3],
 });
 
-const p3 = new Project({ 
+const p4 = new Project({ 
     name: "ADVENT(ur)", 
     ghLink: "https://github.com/William-Schutte/gatsby-react-demo",
     demoLink: "https://wonderful-boyd-e49e91.netlify.app/",
@@ -90,16 +106,6 @@ const p3 = new Project({
         "Finally, I used a bit of GraphQL to manage assets and images."],
     tech: [gatsby, react, graphQL, netlify],
     imgs: [adventur1, adventur2, adventur3],
-});
-
-const p4 = new Project({ 
-    name: "Where to Next?", 
-    ghLink: "https://github.com/William-Schutte/travel-menu",
-    demoLink: "https://wizardly-heyrovsky-06023e.netlify.app/",
-    description: ["A simple React page with destinations I plan on visiting. Destinations can be filtered by region and sorted by flight cost, region, or id (default).",
-        ""],
-    tech: [react, netlify, js],
-    imgs: [travelMenu1, travelMenu2, travelMenu3],
 });
 
 const p5 = new Project({ 
@@ -121,7 +127,15 @@ const p6 = new Project({
     imgs: [portland1, portland2],
 });
 
-
+const p = new Project({ 
+    name: "Where to Next?", 
+    ghLink: "https://github.com/William-Schutte/travel-menu",
+    demoLink: "https://wizardly-heyrovsky-06023e.netlify.app/",
+    description: ["A simple React page with destinations I plan on visiting. Destinations can be filtered by region and sorted by flight cost, region, or id (default).",
+        ""],
+    tech: [react, netlify, js],
+    imgs: [travelMenu1, travelMenu2, travelMenu3],
+});
 
 const projects = [p0, p1, p2, p3, p4, p5, p6];
 export default projects;
